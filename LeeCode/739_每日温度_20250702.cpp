c78c00db7b4be7e61580b7a5b5ffe3e0a98b2b2e
Mugglen <1297183113@qq.com>
2025-07-02 19:01:53 +0800

    20250702

--- LeeCode/739_每日温度_20250702.cpp
+++ LeeCode/739_每日温度_20250702.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 // 通常是一维数组，要寻找任一个元素的右边或者左边第一个比自己大或者小的元素的位置，此时我们就要想到可以用单调栈了
 // 单调栈里只需要存放元素的下标i就可以了，如果需要使用对应的元素，直接T[i]就可以获取。
-#define DEBUG
+//#define DEBUG
 #ifdef DEBUG
 class Solution {
 public:
